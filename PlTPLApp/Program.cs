39d24920e67d397a02d82cc3ce81a3e705a20d25
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-11-30 02:48:02 +0200

    Add project files.

---
+++ PlTPLApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlTPLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
